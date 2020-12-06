c649b0ff130c772ff4c2ec44fe7bc8e3d75916e5
SargeanTravis <69531538+SargeanTravis@users.noreply.github.com>
2020-12-06 08:31:59 -0500

    Add TODO
    Item Producer is a relic from shapez.io beta and is not necessary for final build of the mod. will remove in 1.0 release

--- src/js/game/components/item_producer.js
+++ src/js/game/components/item_producer.js
@@ -1,5 +1,5 @@
 import { Component } from "../component";
-
+//TODO: Remove this from final build
 export class ItemProducerComponent extends Component {
     static getId() {
         return "ItemProducer";
